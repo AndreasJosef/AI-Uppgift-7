2259e22243f162f1b2857a6910b8c783a949ebb7
AndreasJosef <86117013+AndreasJosef@users.noreply.github.com>
2024-11-12 14:59:13 +0100

    Removed empty line

--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -7,7 +7,6 @@ dricks = sns.load_dataset('tips')
 # Quick look at the first few rows of the dataset to know what is in there. 
 print(dricks.head())
 
-
 # Check for missing values
 print("Missing values:\n", dricks.isnull().sum())
 
